88391f56b492674c99de2374dfce2cf31b4eb7a2
jaswanth <chjaswanth321@gmail.com>
2022-12-06 11:06:24 +0530

    push it

--- scripts/script.js
+++ scripts/script.js
@@ -7,16 +7,16 @@ check.addEventListener('click',function(e){
     let head = document.getElementById('head')
     let user_img = document.getElementById('img')
     console.log('this button is clicked')
-    if (user_img.style.display=='none' && head.style.display=='none'){
+    // if (user_img.style.display=='none' && head.style.display=='none'){
         
-        user_img.style.display='block'
-        head.style.display='block'
-    }
-    else{
+    //     user_img.style.display='block'
+    //     head.style.display='block'
+    // }
+    // else{
         
-        user_img.style.display='none'
-        head.style.display='none'
-    }
+    //     user_img.style.display='none'
+    //     head.style.display='none'
+    // }
     // user_img.Visiblity = toggleButton.Ischecked ? Visiblity.Visiable : Visiblity.Collapsed;
     
 })
--- scripts/second_script.js
+++ scripts/second_script.js
@@ -4,12 +4,12 @@ check.addEventListener('click',function(e){
     let ul = document.getElementById('ul')
     let group_pic = document.getElementById('group_pic')
     
-    if (group_pic.style.display=='none'){
-        group_pic.style.display='block'
+    // if (group_pic.style.display=='none'){
+    //     group_pic.style.display='block'
     
-    }
-    else{
-        group_pic.style.display='none'
-    }
+    // }
+    // else{
+    //     group_pic.style.display='none'
+    // }
     
 })
